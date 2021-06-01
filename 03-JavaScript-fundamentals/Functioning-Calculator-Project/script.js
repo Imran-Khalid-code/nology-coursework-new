0cb4524ca5da22dbd6f27b9c231c0769a09ae19b
imran <ikhalidf7@gmail.com>
2021-06-01 14:51:49 +0100

    HTML , CSS & JS created

---
+++ 03-JavaScript-fundamentals/Functioning-Calculator-Project/script.js
@@ -0,0 +1,49 @@
+
+// need to interact with DOM by using quesry selctors to access the different button on calc - Display:
+const display1 = document.querySelector('.display-1');
+const display2 = document.querySelector('.display-2');
+const tempResult = document.querySelector('temp-result');
+
+// Math operations - reduce code by making one overall function call
+
+const mathCalcs = document.querySelector('.math-Calcs');
+
+
+//micellaneous:
+const allNumbers = document.querySelectorAll('.calcNumber');
+const allZero = document.querySelector('.zero');
+const allDot= document.querySelector('.dot');
+const allEquals= document.querySelector('.equals');
+const all = document.querySelector('.last-entity-clear');
+const allClear = document.querySelector('.is-clear');
+
+
+
+//running display variables:
+let dis1Num = "";
+let dis2Num = "";
+let result = null;
+let lastOperation = "";
+let haveDot = false;
+
+
+//method to check if the number already has a dot using eventlistener and event object:
+
+allNumbers.forEach((calcNumber)=> { 
+    calcNumber.addEventListener("click", (event) => {
+        if(event.target.innerText === "." && !haveDot) {
+            haveDot = true;
+        } else if (event.target.innerText === "." && haveDot)
+    {
+        return;
+
+        }
+        
+// whenever we click a number, we want to display it as our display2 variable:
+        dis2Num += event.target.innerText;
+        display2.innerText = dis2Num;
+    });
+});
+
+// math operation functions checking for dot:
+
