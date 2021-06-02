9741e3d05f990a424d8d9563546760626efe699a
imran <ikhalidf7@gmail.com>
2021-06-02 23:11:53 +0100

    added keyboard functionality

--- 03-JavaScript-fundamentals/Functioning-Calculator-Project/script.js
+++ 03-JavaScript-fundamentals/Functioning-Calculator-Project/script.js
@@ -130,7 +130,45 @@ function clearVar(mathCalcsName = ""){
   });
 
 
+// keyboard functionally operation:
 
+
+window.addEventListener("keydown", (event) => {
+    if (
+      event.key === "0" ||
+      event.key === "1" ||
+      event.key === "2" ||
+      event.key === "3" ||
+      event.key === "4" ||
+      event.key === "5" ||
+      event.key === "6" ||
+      event.key === "7" ||
+      event.key === "8" ||
+      event.key === "9" ||
+      event.key === "." ||
+      event.key === "*" ||
+      event.key === "/" ||
+      event.key === "+" ||
+      event.key === "-" ||
+      event.key === "%" ||
+      event.key === "=" ||
+      //press tab C to delete:
+      event.key === "C" 
+    
+    ) {
+      clickButton(event.key);
+    }
+     
+ 
+  });
+
+  function clickButton(key) {
+    allNumbers.forEach((button) => {
+      if (button.innerText === key) {
+        button.click();
+      }
+    });
+  };
 
 
  
