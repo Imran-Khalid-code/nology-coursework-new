5e75ef6fbf26f984fc3fad24397a5ceb183e3349
imran <ikhalidf7@gmail.com>
2021-06-09 22:35:59 +0100

    gameboard desgined using Grid and flexbox

--- 03-JavaScript-fundamentals/Tic-Tac-Toe-Project/dist/main.dev.js
+++ 03-JavaScript-fundamentals/Tic-Tac-Toe-Project/dist/main.dev.js
@@ -1,31 +1,18 @@
+"use strict";
 
 //pseudocode logic:
-
 //need to add a selctor to acces all the button
 // need to add an event listner to get buttons to respond to being pressed
 //need to add call back functions for :
-
-
-
 //Need to find away to start with a clear board that take in a 0 or X input.
 //need a function to randomly give X or O at the start of each game.
 //when each player enters a button it takes it in and stores it 
 //a function that stops you entering wrong key - if statement
 // a function that stops the game as soon as there is three in a row
 // a function that empty the board once we reach the end 
-
 //need t0 add a default for to stop refresh in order to start a new game.
-
-
 //Running variables:
-
-let gameBoardReset = "";
-let haveX = false;
-let haveO = false;
-let result= null;
-
-
-
-
-
-
+var gameBoardReset = "";
+var haveX = false;
+var haveO = false;
+var result = null;
