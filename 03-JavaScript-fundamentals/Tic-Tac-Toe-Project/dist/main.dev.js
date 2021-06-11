c2b922c83ada26a67251e0b9b3b94e72064596d8
imran <ikhalidf7@gmail.com>
2021-06-11 07:29:56 +0100

    eventListener added to interact with the DOM & array.from used to build grid to allow for indexing

--- 03-JavaScript-fundamentals/Tic-Tac-Toe-Project/dist/main.dev.js
+++ 03-JavaScript-fundamentals/Tic-Tac-Toe-Project/dist/main.dev.js
@@ -1,18 +1,27 @@
 "use strict";
 
 //pseudocode logic:
-//need to add a selctor to acces all the button
-// need to add an event listner to get buttons to respond to being pressed
-//need to add call back functions for :
-//Need to find away to start with a clear board that take in a 0 or X input.
-//need a function to randomly give X or O at the start of each game.
-//when each player enters a button it takes it in and stores it 
-//a function that stops you entering wrong key - if statement
-// a function that stops the game as soon as there is three in a row
-// a function that empty the board once we reach the end 
-//need t0 add a default for to stop refresh in order to start a new game.
-//Running variables:
-var gameBoardReset = "";
-var haveX = false;
-var haveO = false;
-var result = null;
+//1- need to make a board - use an array
+//2- interact with DOM - so the buttons are operational
+//3- have a place holder so we know who goes first
+//4- swap turns
+//5- dont allow to put in the same place if there is already an element there
+//6- have a message saying - you won
+//7- no need to refresh when you start a new game
+var allSquares = document.querySelectorAll('.space');
+allSquares.forEach(function (space) {
+  space.addEventListener('click', hitTheButton); // console.log('clicked');
+}); //need to turn our 9 squares into an array - in-built method called array.from can perform this action for us, which we can then index
+//the index will tell the user which space they are clicking on our grid.
+
+function hitTheButton(event) {
+  var spaceArray = Array.from(allSquares);
+  var findIndex = spaceArray.indexOf(event.target);
+  console.log(findIndex);
+} //need to decide who is going to play first.
+
+
+var currentPlayer = 'playerZero';
+var playerSelection = document.querySelector('player'); //now we need to display this player in the console - use innerHTML for this.
+
+playerSelection.innerHTML += currentPlayer; //need an if statment to swap players & and then stop from pressing on the same place twice
--- 03-JavaScript-fundamentals/Tic-Tac-Toe-Project/main.js
+++ 03-JavaScript-fundamentals/Tic-Tac-Toe-Project/main.js
@@ -1,31 +1,42 @@
-
 //pseudocode logic:
 
-//need to add a selctor to acces all the button
-// need to add an event listner to get buttons to respond to being pressed
-//need to add call back functions for :
+//1- need to make a board - use an array
+//2- interact with DOM - so the buttons are operational
+//3- have a place holder so we know who goes first
+//4- swap turns
+//5- dont allow to put in the same place if there is already an element there
+//6- have a message saying - you won
+//7- no need to refresh when you start a new game
 
 
+const allSquares = document.querySelectorAll('.space');
 
-//Need to find away to start with a clear board that take in a 0 or X input.
-//need a function to randomly give X or O at the start of each game.
-//when each player enters a button it takes it in and stores it 
-//a function that stops you entering wrong key - if statement
-// a function that stops the game as soon as there is three in a row
-// a function that empty the board once we reach the end 
+allSquares.forEach((space)=> {
 
-//need t0 add a default for to stop refresh in order to start a new game.
+    space.addEventListener('click', hitTheButton);
+    // console.log('clicked');
+})
 
+//need to turn our 9 squares into an array - in-built method called array.from can perform this action for us, which we can then index
+//the index will tell the user which space they are clicking on our grid.
 
-//Running variables:
-
-let gameBoardReset = "";
-let haveX = false;
-let haveO = false;
-let result= null;
-
+function hitTheButton(event) {
+    const spaceArray = Array.from(allSquares)
+    const findIndex = spaceArray.indexOf(event.target)
+    console.log(findIndex);
+}
 
 
+//need to decide who is going to play first.
 
+let currentPlayer =  'playerZero'; 
+
+const playerSelection = document.querySelector('player');
+
+//now we need to display this player in the console - use innerHTML for this.
+
+playerSelection.innerHTML += currentPlayer;
+
+//need an if statment to swap players & and then stop from pressing on the same place twice
 
 
